bc7bfe6e4309cb092602f97c62e99d828665abe5
Ayyanar <48320634+ayyanar-codes@users.noreply.github.com>
2024-09-03 09:15:40 +0530

    Add files via upload

---
+++ Dependency-Injection/dependency-injection-example1/dependency-injection-used-by-method/dependency-injection-example/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dependency_injection_example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
